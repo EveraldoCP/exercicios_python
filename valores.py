9d934d6bbebc95d89d79ddbc6ce19fc58bfe0359
EveraldoCP <everaldopedroso73@gmail.com>
2026-08-18 23:59:37 -0300

    exercicio de soma de listas

---
+++ valores.py
@@ -0,0 +1,5 @@
+gastos = [2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08]
+
+total = len(gastos)
+media = sum(gastos)/total
+print(f'Essa é a média: {media:.2f}')
